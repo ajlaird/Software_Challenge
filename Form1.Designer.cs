aad61bd4798a8071ece076b5600018a836f28e78
ajlaird <ajlaird.96@gmail.com>
2017-02-07 09:22:41 +0000

    SoftwareChallenge

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Software_Challenge_App
+{
+    partial class RoomCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWidth1 = new System.Windows.Forms.TextBox();
+            this.txtWidth2 = new System.Windows.Forms.TextBox();
+            this.txtWidth4 = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWidth1 = new System.Windows.Forms.Label();
+            this.lblWidth2 = new System.Windows.Forms.Label();
+            this.lblWidth4 = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblFloorArea = new System.Windows.Forms.Label();
+            this.lblPaint = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblWidth3 = new System.Windows.Forms.Label();
+            this.txtWidth3 = new System.Windows.Forms.TextBox();
+            this.rdoMetres = new System.Windows.Forms.RadioButton();
+            this.rdoFeet = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(140, 84);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 0;
+            this.txtHeight.Text = "0";
+            // 
+            // txtWidth1
+            // 
+            this.txtWidth1.Location = new System.Drawing.Point(140, 135);
+            this.txtWidth1.Name = "txtWidth1";
+            this.txtWidth1.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth1.TabIndex = 1;
+            this.txtWidth1.Text = "0";
+            // 
+            // txtWidth2
+            // 
+            this.txtWidth2.Location = new System.Drawing.Point(140, 184);
+            this.txtWidth2.Name = "txtWidth2";
+            this.txtWidth2.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth2.TabIndex = 2;
+            this.txtWidth2.Text = "0";
+            // 
+            // txtWidth4
+            // 
+            this.txtWidth4.Location = new System.Drawing.Point(140, 285);
+            this.txtWidth4.Name = "txtWidth4";
+            this.txtWidth4.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth4.TabIndex = 3;
+            this.txtWidth4.Text = "0";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(30, 85);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(41, 13);
+            this.lblHeight.TabIndex = 4;
+            this.lblHeight.Text = "Height:";
+            // 
+            // lblWidth1
+            // 
+            this.lblWidth1.AutoSize = true;
+            this.lblWidth1.Location = new System.Drawing.Point(30, 135);
+            this.lblWidth1.Name = "lblWidth1";
+            this.lblWidth1.Size = new System.Drawing.Size(47, 13);
+            this.lblWidth1.TabIndex = 5;
+            this.lblWidth1.Text = "Width 1:";
+            // 
+            // lblWidth2
+            // 
+            this.lblWidth2.AutoSize = true;
+            this.lblWidth2.Location = new System.Drawing.Point(30, 185);
+            this.lblWidth2.Name = "lblWidth2";
+            this.lblWidth2.Size = new System.Drawing.Size(47, 13);
+            this.lblWidth2.TabIndex = 6;
+            this.lblWidth2.Text = "Width 2:";
+            // 
+            // lblWidth4
+            // 
+            this.lblWidth4.AutoSize = true;
+            this.lblWidth4.Location = new System.Drawing.Point(30, 285);
+            this.lblWidth4.Name = "lblWidth4";
+            this.lblWidth4.Size = new System.Drawing.Size(47, 13);
+            this.lblWidth4.TabIndex = 7;
+            this.lblWidth4.Text = "Width 4:";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(28, 389);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(0, 13);
+            this.lblVolume.TabIndex = 8;
+            // 
+            // lblFloorArea
+            // 
+            this.lblFloorArea.AutoSize = true;
+            this.lblFloorArea.Location = new System.Drawing.Point(26, 321);
+            this.lblFloorArea.Name = "lblFloorArea";
+            this.lblFloorArea.Size = new System.Drawing.Size(0, 13);
+            this.lblFloorArea.TabIndex = 9;
+            // 
+            // lblPaint
+            // 
+            this.lblPaint.AutoSize = true;
+            this.lblPaint.Location = new System.Drawing.Point(28, 357);
+            this.lblPaint.Name = "lblPaint";
+            this.lblPaint.Size = new System.Drawing.Size(0, 13);
+            this.lblPaint.TabIndex = 10;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(304, 428);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblWidth3
+            // 
+            this.lblWidth3.AutoSize = true;
+            this.lblWidth3.Location = new System.Drawing.Point(30, 235);
+            this.lblWidth3.Name = "lblWidth3";
+            this.lblWidth3.Size = new System.Drawing.Size(47, 13);
+            this.lblWidth3.TabIndex = 14;
+            this.lblWidth3.Text = "Width 3:";
+            // 
+            // txtWidth3
+            // 
+            this.txtWidth3.Location = new System.Drawing.Point(140, 235);
+            this.txtWidth3.Name = "txtWidth3";
+            this.txtWidth3.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth3.TabIndex = 3;
+            this.txtWidth3.Text = "0";
+            // 
+            // rdoMetres
+            // 
+            this.rdoMetres.AutoSize = true;
+            this.rdoMetres.Checked = true;
+            this.rdoMetres.Location = new System.Drawing.Point(91, 428);
+            this.rdoMetres.Name = "rdoMetres";
+            this.rdoMetres.Size = new System.Drawing.Size(57, 17);
+            this.rdoMetres.TabIndex = 15;
+            this.rdoMetres.TabStop = true;
+            this.rdoMetres.Text = "Metres";
+            this.rdoMetres.UseVisualStyleBackColor = true;
+            // 
+            // rdoFeet
+            // 
+            this.rdoFeet.AutoSize = true;
+            this.rdoFeet.Location = new System.Drawing.Point(91, 452);
+            this.rdoFeet.Name = "rdoFeet";
+            this.rdoFeet.Size = new System.Drawing.Size(46, 17);
+            this.rdoFeet.TabIndex = 16;
+            this.rdoFeet.Text = "Feet";
+            this.rdoFeet.UseVisualStyleBackColor = true;
+            // 
+            // RoomCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 526);
+            this.Controls.Add(this.rdoFeet);
+            this.Controls.Add(this.rdoMetres);
+            this.Controls.Add(this.lblWidth3);
+            this.Controls.Add(this.txtWidth3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblPaint);
+            this.Controls.Add(this.lblFloorArea);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblWidth4);
+            this.Controls.Add(this.lblWidth2);
+            this.Controls.Add(this.lblWidth1);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtWidth4);
+            this.Controls.Add(this.txtWidth2);
+            this.Controls.Add(this.txtWidth1);
+            this.Controls.Add(this.txtHeight);
+            this.Name = "RoomCalculator";
+            this.Text = "Room Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWidth1;
+        private System.Windows.Forms.TextBox txtWidth2;
+        private System.Windows.Forms.TextBox txtWidth4;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWidth1;
+        private System.Windows.Forms.Label lblWidth2;
+        private System.Windows.Forms.Label lblWidth4;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblFloorArea;
+        private System.Windows.Forms.Label lblPaint;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblWidth3;
+        private System.Windows.Forms.TextBox txtWidth3;
+        private System.Windows.Forms.RadioButton rdoMetres;
+        private System.Windows.Forms.RadioButton rdoFeet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Challenge_App
+{
+    public partial class RoomCalculator : Form
+    {
+        public bool valid { get; private set; } //boolean which is checked to ensure user entered data is valid
+
+        public RoomCalculator()
+        {
+            InitializeComponent();
+        }
+
+
+        public void validate(string var) //validation method which checks user entered data to ensure it is only numerical data
+        {
+            
+            if (var == null)
+            {
+                valid = false;
+            }
+            else if (var == "0")
+            {
+                valid = false;
+            }
+            bool allDigits = var.All(char.IsDigit);
+            if (allDigits == false)
+            {
+                valid = false;
+            }
+        }
+
+        public void calcVolume() //method which calulates volume based on measurements
+        {
+            double height = Convert.ToInt32(txtHeight.Text);
+            double width1 = Convert.ToInt32(txtWidth1.Text);
+            double width2 = Convert.ToInt32(txtWidth2.Text);
+            double width3 = Convert.ToInt32(txtWidth3.Text);
+            double width4 = Convert.ToInt32(txtWidth4.Text);
+            double s = (width1 + width2 + width3 + width4) / 2;
+            double floorArea = Math.Sqrt((s - width1) * (s - width2) * (s - width3) * (s - width4)); //formula to calculate area of quadrilateral
+            double volume = Math.Round((floorArea * height), 2); //multiplies area by height to return volume
+            if (rdoFeet.Checked == true)//If the user entered measurements in feet, multiply result by 3.28
+                {
+                volume = Math.Round((volume * 3.28), 2);
+                lblVolume.Text = "Volume of room(Feet Cubed):" + Convert.ToString(volume);//display result
+            }
+
+            else
+            {
+                lblVolume.Text = "Room Volume(Metres Cubed):" + Convert.ToString(volume);//display result
+            }
+        }
+
+        public void calcArea()
+        {
+            double height = Convert.ToInt32(txtHeight.Text);
+            double width1 = Convert.ToInt32(txtWidth1.Text);
+            double width2 = Convert.ToInt32(txtWidth2.Text);
+            double width3 = Convert.ToInt32(txtWidth3.Text);
+            double width4 = Convert.ToInt32(txtWidth4.Text);
+            double s = (width1 + width2 + width3 + width4) / 2; 
+            double floorArea = Math.Round(Math.Sqrt((s - width1) * (s - width2) * (s - width3) * (s - width4)),2); //formula to calculate area of quadrilateral
+            if (rdoFeet.Checked == true) //If the user entered measurements in feet, multiply result by 3.28
+
+                {
+                floorArea = Math.Round(floorArea * 3.28,2);
+                lblFloorArea.Text = "Floor Area(Feet Sqaured):" + Convert.ToString(floorArea);//display result
+                }
+
+            else
+            {
+                lblFloorArea.Text = "Floor Area(Metres Squared):" + Convert.ToString(floorArea);//display result
+                }
+        }
+
+        public void calcPaint()
+        {
+            int height = Convert.ToInt32(txtHeight.Text);
+            int width1 = Convert.ToInt32(txtWidth1.Text);
+            int width2 = Convert.ToInt32(txtWidth2.Text);
+            int width3 = Convert.ToInt32(txtWidth3.Text);
+            int width4 = Convert.ToInt32(txtWidth4.Text);
+            int wallArea = (width1 + width2 + width3 + width4) * height;
+            double paint = Math.Round(wallArea * 0.1, 2);
+            if (rdoFeet.Checked == true)
+            {
+                paint = Math.Round(paint * 3.28, 2); //If the user entered measurements in feet, multiply result by 3.28
+            }
+
+            lblPaint.Text = "Amount of paint(Litres):" + Convert.ToString(paint); //display result
+
+        }
+        public void btnSubmit_Click(object sender, EventArgs e)
+        {
+            valid = true;
+            validate(txtHeight.Text); //validates fields
+            validate(txtWidth1.Text);
+            validate(txtWidth2.Text);
+            validate(txtWidth3.Text);
+            validate(txtWidth4.Text);
+            if (valid == true)
+            {
+                calcVolume();
+                calcPaint();
+                calcArea();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid measurements", "Invalid Measurements", MessageBoxButtons.OK, MessageBoxIcon.Error); //error is displayed if validation finds invalid data
+            }
+            
+        }
+
+    }
+}
